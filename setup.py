c47b2ce5e302e5b4199504ec8c78d6dcd7549948
Takuya Murakami <murakami@da.jp.nec.com>
2017-07-01 18:26:11 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='nebula',
+      version='0.1',
+      packages=['nebula'],
+      description='NEC Mobile Backend platform Python SDK',
+      author='NEC Corporation',
+      )
