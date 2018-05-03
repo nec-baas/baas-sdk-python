d5c6b330b88b6a45fe1625140c756e3e5d7f5838
Takuya Murakami <murakami@da.jp.nec.com>
2018-05-03 22:02:27 +0900

    add test/doc requires

--- setup.py
+++ setup.py
@@ -7,12 +7,25 @@ requires = [
     'requests>=2.18.0'
 ]
 
+test_requires = [
+    'nose'
+]
+
+doc_requires = [
+    'sphinx',
+    'sphinx-rtd-theme'
+]
+
 setup(
     name='necbaas',
     version='0.0.2',
     packages=['necbaas'],
     description='NEC Mobile Backend platform Python SDK',
     author='NEC Corporation',
-    install_requires=requires
+    install_requires=requires,
+    extra_requires={
+        'test': test_requires,
+        'doc': doc_requires
+    }
 )
 
