0a9eeff49eb12ced76fb804f09440aea80dd87b7
Takuya Murakami <murakami@da.jp.nec.com>
2018-05-09 22:54:06 +0900

    bug fix Apigw: json引数渡し漏れ修正

--- necbaas/apigw.py
+++ necbaas/apigw.py
@@ -59,4 +59,4 @@ class Apigw(object):
         path = self.apiname
         if self.subpath is not None:
             path = path + "/" + self.subpath
-        return self.service.execute_rest(self.method, path, query=query, data=data, headers=headers)
+        return self.service.execute_rest(self.method, path, query=query, data=data, json=json, headers=headers)
