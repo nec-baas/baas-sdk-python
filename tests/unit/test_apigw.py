4d029c170fb0c68756b5a9246c25ff83c7a8f7f6
Takuya Murakami <murakami@da.jp.nec.com>
2018-05-21 15:25:51 +0900

    add UT: apigw

---
+++ tests/unit/test_apigw.py
@@ -0,0 +1,24 @@
+import necbaas as baas
+
+from .util import *
+
+
+class TestApigw(object):
+    def test_execute(self):
+        """正常に APIGW コールできること"""
+        expected_result ={"result": 1}
+        service = mock_service_json_resp(expected_result)
+
+        apigw = baas.Apigw(service, "api1", "POST", "/a/b/c")
+
+        json = {"key1": 12345}
+        query ={"query": "xxx"}
+        headers = {"X-Some-Header": "xxxxx"}
+        result = apigw.execute(json=json, query=query, headers=headers).json()
+        assert result == expected_result
+
+        assert get_rest_args(service) == ("POST", "api1/a/b/c")
+        kwargs = get_rest_kwargs(service)
+        assert kwargs["json"] == json
+        assert kwargs["query"] == query
+        assert kwargs["headers"] == headers
