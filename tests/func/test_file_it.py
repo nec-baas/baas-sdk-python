99ffe6264988d067d007aea2cad162ce76feca20
Takuya Murakami <murakami@da.jp.nec.com>
2018-05-22 09:31:38 +0900

    update FT: file data

--- tests/func/test_file_it.py
+++ tests/func/test_file_it.py
@@ -10,23 +10,31 @@ class TestFileStorage(TestStorageBase):
     def teardown(self):
         self.cleanup()
 
+    def create_test_data(self, length):
+        ary = []
+        for i in range(length):
+            alphabet = "abcdefghijklmnopqrstuvwxyz"
+            idx = i % len(alphabet)
+            ary.append(alphabet[idx:idx+1])
+        return ''.join(ary)
+
     def test_upload_download(self):
         b = baas.FileBucket(self.service, "bucket1")
 
         # upload
-        data = "abcde".encode()
-        meta = b.upload("file1.txt", data=data, content_type="plain/text")
+        test_data = self.create_test_data(1024)
+        meta = b.upload("file1.txt", data=test_data.encode(), content_type="plain/text")
         assert meta["filename"] == "file1.txt"
-        assert meta["length"] == 5
+        assert meta["length"] == len(test_data)
         assert meta["contentType"] == "plain/text"
 
         # download
         res = b.download("file1.txt")
         assert res.status_code == 200
-        assert res.text == "abcde"
+        assert res.text == test_data
 
         # stream download
         with b.download("file1.txt", stream=True) as res:
             assert res.status_code == 200
             data = res.raw.read()
-            assert data.decode() == "abcde"
+            assert data.decode() == test_data
