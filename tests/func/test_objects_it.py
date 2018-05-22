e2667e26799bbd7b8648f0553ba606dca929a246
Takuya Murakami <murakami@da.jp.nec.com>
2018-05-22 09:09:10 +0900

    add FT: object

--- tests/func/test_objects_it.py
+++ tests/func/test_objects_it.py
@@ -9,6 +9,16 @@ class TestObjectStorage(TestStorageBase):
     def teardown(self):
         self.cleanup()
 
+    def insert_sample_data(self):
+        # type: () -> baas.ObjectBucket
+        b = baas.ObjectBucket(self.service, "bucket1")
+
+        for i in range(10):
+            #print(i)
+            b.insert({"key": i})
+
+        return b
+
     def test_crud(self):
         b = baas.ObjectBucket(self.service, "bucket1")
 
@@ -25,11 +35,6 @@ class TestObjectStorage(TestStorageBase):
         assert len(results) == 1
         assert results[0]["key1"] == 23456
 
-        # query with count
-        (results, count) = b.query_with_count()
-        assert len(results) == 1
-        assert count == 1
-
         # remove
         _id = results[0]["_id"]
         res = b.remove(_id)
@@ -39,3 +44,31 @@ class TestObjectStorage(TestStorageBase):
         res = b.remove_with_query({})
         assert res["result"] == "ok"
         assert res["deletedObjects"] == 0
+
+    def test_query_all(self):
+        """全件検索できること"""
+        b = self.insert_sample_data()
+
+        results = b.query()
+        assert len(results) == 10
+
+    def test_query_with_condition(self):
+        """条件付きクエリできること"""
+        b = self.insert_sample_data()
+        where = {"key": {"$lt": 9}}
+        projection = {"createdAt": 0}
+        results = b.query(where=where, order="-key", skip=2, limit=5, projection=projection)
+        assert len(results) == 5  # limit
+        assert results[0]["key"] == 6  # where, order, skip
+        assert results[4]["key"] == 2
+        assert "createdAt" not in results[0]  # projection
+        assert "updatedAt" in results[0]
+
+    def test_query_with_count(self):
+        """件数付きクエリできること"""
+        b = self.insert_sample_data()
+
+        (results, count) = b.query_with_count(limit=5)
+        assert len(results) == 5
+        assert count == 10
+
