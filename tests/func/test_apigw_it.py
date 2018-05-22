317164c807d581c58fc8f5d21d6c53b9b39e0b14
Takuya Murakami <murakami@da.jp.nec.com>
2018-05-22 15:54:51 +0900

    update FT: Apigw

--- tests/func/test_apigw_it.py
+++ tests/func/test_apigw_it.py
@@ -6,6 +6,9 @@ from requests import HTTPError
 import necbaas as baas
 from . import util
 
+# ロガー初期化
+logging.basicConfig(level=logging.WARNING)
+
 
 class TestApigw(object):
     service = None
@@ -16,6 +19,8 @@ class TestApigw(object):
 
     def test_execute_nonexist(self):
         """存在しない APIGW 呼び出し。404 Not Found となること"""
+        self.service.logger.setLevel(logging.DEBUG)  # REST API log
+
         apigw = baas.Apigw(self.service, "api1", "GET", "a/b/c/d/e/f/g")
 
         with pytest.raises(HTTPError) as ei:
