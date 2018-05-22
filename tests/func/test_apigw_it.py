159cafc2a438f2b38ab102ab25027a253f63a565
Takuya Murakami <murakami@da.jp.nec.com>
2018-05-22 15:53:00 +0900

    add FT: logger

--- tests/func/test_apigw_it.py
+++ tests/func/test_apigw_it.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import pytest
+import logging
 from requests import HTTPError
 
 import necbaas as baas
---
+++ tests/func/test_logger.py
@@ -0,0 +1,19 @@
+import logging
+import necbaas as baas
+from . import util
+
+# ロガー初期化
+logging.basicConfig(level=logging.WARNING)
+
+
+class TestLogger(object):
+    master_service = None
+    # type: baas.Service
+
+    def setup(self):
+        self.master_service = util.create_service(master=True)
+
+    def test_debug_log(self):
+        """デバッグログが出力されること(手動テスト、目視確認すること)"""
+        self.master_service.logger.setLevel(logging.DEBUG)
+        baas.User.query(self.master_service)
